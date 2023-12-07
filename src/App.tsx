5df343ecf4391e4f46ad2c7e1b2637afc3de27b5
JAYBORICHA <jayboricha707@gmail.com>
2023-12-07 23:21:49 +0530

    Editing is left

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import {
   MaterialReactTable,
   // createRow,
   type MRT_ColumnDef,
-  type MRT_Row,
-  type MRT_TableOptions,
 } from 'material-react-table';
 import {
   Box,
@@ -19,8 +17,6 @@ import {
 import {
   QueryClient,
   QueryClientProvider,
-  useMutation,
-  useQueryClient,
 } from '@tanstack/react-query';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -97,84 +93,11 @@ const App = () => {
       [validationErrors],
       )
       
-      const validateRequired = (value: string) => !!value.length;
-      
-      function validateUser(user: userData) {
-        return {
-          firstName: !validateRequired(user.name)
-          ? 'First Name is Required'
-          : '',
-        };
-      }
-      
-      function useUpdateUser() {
-        const queryClient = useQueryClient();
-        return useMutation({
-          mutationFn: async (user: userData) => {
-            //send api update request here
-            await new Promise((resolve) => setTimeout(resolve, 1000)); //fake api call
-            return Promise.resolve();
-          },
-          //client side optimistic update
-          onMutate: (newUserInfo: userData) => {
-            queryClient.setQueryData(
-              ['users'],
-              (prevUsers: userData[]) =>
-                prevUsers?.map((prevUser: userData) =>
-                  prevUser.id === newUserInfo.id ? newUserInfo : prevUser,
-                ),
-            );
-          },
-          // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users'] })
-        });
-      }
+    const handleSaveUser = (e : any) => {
+      const index = allUserData!.findIndex((user) => {user.id == e?.value?.id})!
+      if(index != -1) setAllUserData(allUserData![index] = e.value.id)
+    }
 
-      const { mutateAsync: updateUser, isPending: isUpdatingUser } =useUpdateUser();
-    const handleSaveUser: MRT_TableOptions<userData>['onEditingRowSave'] = async ({
-      values,
-      table,
-    }) => {
-      const newValidationErrors = validateUser(values);
-      if (Object.values(newValidationErrors).some((error) => error)) {
-        setValidationErrors(newValidationErrors);
-        return;
-      }
-      setValidationErrors({});
-      await updateUser(values);
-      table.setEditingRow(null); //exit editing mode
-    };
-  
-  
-    // function useDeleteUser() {
-    //   const queryClient = useQueryClient();
-    //   return useMutation({
-    //     mutationFn: async (userId: string) => {
-    //       //send api update request here
-    //       await new Promise((resolve) => setTimeout(resolve, 1000)); //fake api call
-    //       return Promise.resolve();
-    //     },
-    //     //client side optimistic update
-    //     onMutate: (userId: string) => {
-    //       queryClient.setQueryData(
-    //         ['users'],
-    //         (prevUsers: userData[]) =>
-    //           prevUsers?.filter((user: userData) => user.id !== userId),
-    //       );
-    //     },
-    //     // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users'] }), //refetch users after mutation, disabled for demo
-    //   });
-    // }
-
-    
-    // //call DELETE hook
-    // const { mutateAsync: deleteUser, isPending: isDeletingUser } =
-    // useDeleteUser();
-
-    // const openDeleteConfirmModal = (row: MRT_Row<userData>) => {
-    //   if (window.confirm('Are you sure you want to delete this user?')) {
-    //     deleteUser(row.original.id);
-    //   }
-    // };
     return(
       <QueryClientProvider client={queryClient}>
         {allUserData && 
@@ -187,35 +110,36 @@ const App = () => {
         getRowId={(row)=>row.id}
         onCreatingRowCancel={()=>{setValidationErrors({})}}
         onEditingRowCancel={()=>{setValidationErrors({})}}
-        // onEditingRowSave={handleSaveUser}
-        // renderEditRowDialogContent = {({ table, row, internalEditComponents })=>{
-        //   return(
-        //   <>
-        //     <DialogTitle variant="h3">Edit User</DialogTitle>
-        //     <DialogContent
-        //       sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
-        //     >
-        //       {internalEditComponents} {/* or render custom edit components here */}
-        //     </DialogContent>
-        //     <DialogActions>
-        //       <MRT_EditActionButtons variant="text" table={table} row={row} />
-        //     </DialogActions>
-        //   </>
-        //   )}}
-        // renderRowActions={({ row, table })=>(
-        //   <Box sx={{ display: 'flex', gap: '1rem' }}>
-        //     <Tooltip title="Edit">
-        //       <IconButton onClick={() => table.setEditingRow(row)}>
-        //         <EditIcon />
-        //       </IconButton>
-        //     </Tooltip>
-        //     <Tooltip title="Delete">
-        //       <IconButton color="error" onClick={() => openDeleteConfirmModal(row)}>
-        //         <DeleteIcon />
-        //       </IconButton>
-        //     </Tooltip>
-        //   </Box>
-        // )}
+        onEditingRowSave={(e) => {handleSaveUser(e)}}
+        renderEditRowDialogContent = {({ table, row, internalEditComponents })=>{
+          return(
+          <>
+            <DialogTitle variant="h3">Edit User</DialogTitle>
+            <DialogContent
+              sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
+            >
+              {internalEditComponents}
+            </DialogContent>
+            <DialogActions>
+              <MRT_EditActionButtons variant="text" table={table} row={row} />
+            </DialogActions>
+          </>
+          )}
+        }
+        renderRowActions={({ row, table })=>(
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Tooltip title="Edit">
+              <IconButton onClick={() => table.setEditingRow(row)}>
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Delete">
+              <IconButton color="error" onClick={() => setAllUserData(allUserData.filter((obj) => (obj.id != row.id)))}>
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        )}
       />  
       }
       </QueryClientProvider>
